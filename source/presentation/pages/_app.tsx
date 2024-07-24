7bc4f7ba562decc744d9e183be10dbec0edd9974
Shiro <jtl77rafael@gmail.com>
2024-07-24 18:04:41 -0300

    feat: configure the project fonts

--- source/presentation/pages/_app.tsx
+++ source/presentation/pages/_app.tsx
@@ -1,7 +1,33 @@
 import "@/presentation/styles/global.css";
 
+import { Montserrat, Spectral } from "next/font/google";
+import { Fragment } from "react";
+
 import type { AppProps } from "next/app";
 
+const base = Montserrat({
+  subsets: ["latin"],
+  variable: "--font-base",
+  weight: ["400", "500", "700", "800"],
+});
+
+const primary = Spectral({
+  subsets: ["latin"],
+  variable: "--font-primary",
+  weight: ["200", "300", "400", "500"],
+});
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Fragment>
+      <style jsx global>{`
+        body {
+          --font-base: ${base.style.fontFamily};
+          --font-primary: ${primary.style.fontFamily};
+        }
+      `}</style>
+
+      <Component {...pageProps} />
+    </Fragment>
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,42 +2,14 @@ module.exports = {
   content: [
     "./source/presentation/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./source/presentation/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./source/presentation/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       fontFamily: {
+        base: "var(--font-base)",
         primary: "var(--font-primary)",
       },
     },
   },
-  daisyui: {
-    themes: [
-      {
-        mytheme: {
-          primary: "#111827",
-          "primary-content": "#f3f4f6",
-          secondary: "#312e81",
-          "secondary-content": "#e0e7ff",
-          accent: "#701a75",
-          "accent-content": "#fae8ff",
-          neutral: "#f3f4f6",
-          "neutral-content": "#000",
-          "base-100": "#1f2937",
-          "base-200": "#374151",
-          "base-300": "#4b5563",
-          "base-content": "#f3f4f6",
-          info: "#a5f3fc",
-          "info-content": "#164e63",
-          success: "#bbf7d0",
-          "success-content": "#14532d",
-          warning: "#fde68a",
-          "warning-content": "#7c2d12",
-          error: "#fda4af",
-          "error-content": "#881337",
-        },
-      },
-    ],
-  },
-  plugins: [require("daisyui")],
+  plugins: [],
 };
