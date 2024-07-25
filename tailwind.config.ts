54dbf00ad568b06f991773f8e25bf887e3c44c77
Shiro <jtl77rafael@gmail.com>
2024-07-24 21:34:53 -0300

    feat: add base theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,6 +10,23 @@ module.exports = {
         primary: "var(--font-primary)",
       },
     },
+    colors: {
+      primary: {
+        light: "#9f45b0",
+        DEFAULT: "#44008b",
+      },
+      secondary: {
+        DEFAULT: "#00076f",
+      },
+      accent: {
+        light: "#ffe4f2",
+        DEFAULT: "#e54ed0",
+      },
+      base: {
+        100: "#3F3F3f",
+        DEFAULT: "#000000",
+      },
+    },
   },
   plugins: [],
 };
